6cd804985d2e9008179248d1233627e98b18132d
adilabdubushra <adilabdu68@gmail.com>
2024-03-19 21:52:38 +0300

    Update boilerplate code for Capacitor

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import FooterView from '@/views/FooterView.vue'
 </script>
 
 <template>
-  <div class="fixed flex flex-col items-center justify-center top-0 left-0 right-0 bottom-0 p-4">
+  <div class="pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] fixed flex flex-col items-center justify-center top-0 left-0 right-0 bottom-0 p-4">
     <div class="grow w-full">
       <RouterView />
     </div>
--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -5,6 +5,7 @@ import Heading from '@/components/Heading.vue'
 <template>
 
   <div class="container mx-auto max-w-xl rounded-sm border shadow flex flex-col items-center justify-center h-24">
+    <Heading class="text-[#0E9EFF]" h="3">Capacitor v5</Heading>
     <Heading h="3">Vue 3 + Tailwind CSS 3.4 Boilerplate</Heading>
     <Heading>Pinia + Vue Router included</Heading>
   </div>
