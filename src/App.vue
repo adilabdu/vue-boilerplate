435fdfe9a10f620d32d91bcfaef73d5603e6cab4
adilabdubushra <adilabdu68@gmail.com>
2024-03-21 14:00:55 +0300

    Restyle `App` wrapper component and add padding `FooterView`

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import FooterView from '@/views/FooterView.vue'
 </script>
 
 <template>
-  <div class="fixed flex flex-col items-center justify-center top-0 left-0 right-0 bottom-0 p-4">
+  <div class="overflow-auto flex flex-col items-center justify-center top-0 left-0 right-0 bottom-0 p-4">
     <div class="grow w-full">
       <RouterView />
     </div>
