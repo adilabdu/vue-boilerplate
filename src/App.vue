df0175fb9036d24e56bba3386362cd655f381f70
adilabdubushra <adilabdu68@gmail.com>
2024-03-21 16:48:44 +0300

    Restyle wrapper components

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import FooterView from '@/views/FooterView.vue'
 </script>
 
 <template>
-  <div class="overflow-auto flex flex-col items-center justify-center top-0 left-0 right-0 bottom-0 p-4">
-    <div class="grow w-full">
+  <div class="overflow-auto flex flex-col gap-4 items-center justify-center top-0 left-0 right-0 bottom-0 p-4">
+    <div class="grow flex flex-col gap-4 w-full">
       <RouterView />
     </div>
     <FooterView />
--- src/views/FooterView.vue
+++ src/views/FooterView.vue
@@ -3,8 +3,8 @@ import Heading from '@/components/Heading.vue'
 </script>
 
 <template>
-  <footer class="p-4">
-    <Heading h="6">2024. Adil Abdu Bushra.</Heading>
+  <footer class="sm:text-left text-center w-full max-w-xl">
+    <Heading class="px-2" h="6">2024. Adil Abdu Bushra.</Heading>
   </footer>
 </template>
 
