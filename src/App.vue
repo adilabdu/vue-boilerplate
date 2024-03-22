877fe9e461d77fc08d0182ec58cc222becee65a0
adilabdubushra <adilabdu68@gmail.com>
2024-03-22 22:01:31 +0300

    Add darkmode tailwind classes to for shacn library consistency

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import FooterView from '@/views/FooterView.vue'
 </script>
 
 <template>
-  <div class="overflow-auto flex flex-col gap-4 items-center justify-center top-0 left-0 right-0 bottom-0 p-4">
-    <div class="grow flex flex-col gap-4 w-full">
+  <div class="overflow-auto dark:bg-slate-950 dark:text-slate-50 flex flex-col gap-4 items-center justify-center top-0 left-0 right-0 bottom-0 p-4">
+    <div class="grow flex dark:bg-slate-950 dark:text-slate-50 flex-col gap-4 w-full">
       <RouterView />
     </div>
     <FooterView />
